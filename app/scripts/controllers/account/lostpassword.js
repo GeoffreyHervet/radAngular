2501b67bb5feb9d87b15a03a448d78e93a2d3a3d
Geoffrey Hervet <hervetg@gmail.com>
2015-06-10 16:14:30 +0200

    Lost password

--- app/scripts/controllers/account/lostpassword.js
+++ app/scripts/controllers/account/lostpassword.js
@@ -8,13 +8,40 @@
  * Controller of the angularApp
  */
 angular.module('angularApp')
-  .controller('LostPasswordCtrl', function ($scope, $rootScope) {
+  .controller('LostPasswordCtrl', function ($scope, $rootScope, User, $timeout, $location) {
     $scope.title = 'lostpass.title';
     $rootScope.bodyClass = 'gray';
 
     $scope.email = '';
+    $scope.loading = false;
+    $scope.error = null;
+    $scope.false = true;
 
     $scope.submitForm = function(){
+      $scope.loading = true;
+      $scope.error = null;
+
+      User
+        .forgotPassword($scope.email)
+        .then(function(data){
+          $scope.loading = false;
+          if (data.message.status == 'success') {
+            $scope.success = 'lostpass.success';
+            $scope.error = null;
+            $scope.translateData = {email: $scope.email};
+            $timeout(function(){
+              $location.path('/');
+            }, 5000);
+          }
+          else {
+            $scope.error = data.message.text;
+          }
+
+        }, function(){
+          $scope.loading = false;
+          $scope.error = 'error.connexion_lost';
+        })
+      ;
 
     };
   });
