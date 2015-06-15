33c3667ca6cc25b6946e7c85d831e043dfb79fdd
Geoffrey Hervet <hervetg@gmail.com>
2015-06-15 12:27:33 +0200

    Add order list

--- app/scripts/controllers/account/myorders.js
+++ app/scripts/controllers/account/myorders.js
@@ -8,15 +8,34 @@
  * Controller of the angularApp
  */
 angular.module('angularApp')
-  .controller('MyOrdersCtrl', function ($scope, User, $location) {
+  .controller('MyOrdersCtrl', function ($scope, User, $location, order) {
     if (!User.getToken()) {
       return $location.path('/login');
     }
 
     $scope.loading = true;
     $scope.orders = [];
+    $scope.error = false;
+    $scope.no_orders = false;
+    $scope.title = 'myaccout.myorders.title';
 
+    order.list().then(
+      function (response){
+        $scope.no_orders = false;
+        $scope.loading = false;
+        if (response.message && response.message.status === 'error') {
+          return $scope.error = response.message.text;
+        }
+        if (response.orders && response.orders.item) {
+          return $scope.orders = response.orders.item;
+        }
+        $scope.no_orders = true;
+      },
+      function (){
+        $scope.loading = false;
+        $scope.error = 'error.connexion_lost';
+      }
+    );
 
-    
 
   });
