5e3f50aecb730e306cfbf1633dc4d5031ee33dc8
Geoffrey Hervet <hervetg@gmail.com>
2015-06-15 17:49:14 +0200

    Fix production

--- Gruntfile.js
+++ Gruntfile.js
@@ -442,7 +442,7 @@ module.exports = function (grunt) {
       },
       production: {
         options: {
-          dest: '<%= yeoman.dist %>/scripts/config.js'
+          dest: '<%= yeoman.app %>/scripts/config.js'
         },
         constants: {
           ENV: {
@@ -488,6 +488,7 @@ module.exports = function (grunt) {
 
   grunt.registerTask('build', [
     'clean:dist',
+    'ngconstant:production',
     'wiredep',
     'useminPrepare',
     'concurrent:dist',
@@ -495,7 +496,6 @@ module.exports = function (grunt) {
     'concat',
     'ngAnnotate',
     'copy:dist',
-    'ngconstant:production',
     'cdnify',
     'cssmin',
     'uglify',
