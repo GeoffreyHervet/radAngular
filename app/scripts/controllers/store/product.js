71063ac1c7992d7ad30ca4a579b115b56ad2f340
Geoffrey Hervet <hervetg@gmail.com>
2015-06-18 19:15:16 +0200

    tmp

--- app/scripts/controllers/store/product.js
+++ app/scripts/controllers/store/product.js
@@ -8,14 +8,16 @@
  * Controller of the angularApp
  */
 angular.module('angularApp')
-  .controller('ProductCtrl', function ($scope, $routeParams, $location, Product) {
+  .controller('ProductCtrl', function ($scope, $routeParams, $location, Product, Cart, $timeout) {
     $scope.productId  = parseInt($routeParams.productslug);
     $scope.title      = 'global.loading';
 
     $scope.loading  = true;
+    $scope.error    = false;
     $scope.product  = null;
     $scope.images   = [];
     $scope.options  = [];
+    $scope.quantity = 1;
 
     Product
       .get($scope.productId)
@@ -34,4 +36,46 @@ angular.module('angularApp')
         $scope.loading = false;
       })
     ;
+
+    $scope.changeOption = function(opts, val) {
+      angular.forEach(opts, function(value) {
+        value.active = (val._code == value._code);
+      });
+    };
+
+    $scope.addToCart = function() {
+      $scope.loading = true;
+      Cart
+        .addProduct($scope.productId, $scope.quantity, serializedOptions())
+        .then(function(response){
+          $scope.loading = false;
+          $scope.error = false;
+          if (response.message && response.message.status == 'error') {
+            return $scope.error = response.message.text;
+          }
+          $scope.success = true;
+          $timeout(function(){
+            $scope.success = null;
+          });
+
+        }, function(error){
+          $scope.error = error;
+          $scope.loading = false;
+        })
+      ;
+    };
+
+    var serializedOptions = function(){
+      var ret = {};
+
+      angular.forEach($scope.options, function(option){
+        angular.forEach(option.value, function(val) {
+          if (val.active) {
+            ret[option._code] = val._code;
+          }
+        })
+      });
+
+      return ret;
+    };
   });
