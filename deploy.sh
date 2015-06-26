5608d7b12afb8154b77138b93c2375b80dd40001
Geoffrey Hervet <hervetg@gmail.com>
2015-06-26 15:13:41 +0200

    Add github push for slack notification in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ sleep 10s;
 grunt serve:dist;
 git add dist && git ci -m 'Mise en prod';
 git push origin;
+git push github;
 
 echo '~~~ Deployment ~~~';
 ssh angular@geoffrey.pro "cd ~/www; git pull";
@@ -16,4 +17,4 @@ echo '~~~ Developement constants ~~~';
 grunt ngconstant:development;
 
 echo '~~~ Slack notification ~~~';
-#curl -X POST --data-urlencode 'payload={"channel": "#angular-app", "username": "Script de deploiement", "text": "<@channel>: Une mise à jour est disponible sur http://angular.geoffrey.pro"}' https://hooks.slack.com/services/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
+curl -X POST --data-urlencode 'payload={"channel": "#angular-app", "username": "Script de deploiement", "text": "<@channel>: Une mise à jour est disponible sur http://angular.geoffrey.pro"}' https://hooks.slack.com/services/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
