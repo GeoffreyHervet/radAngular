aa064e08a62e97799b5195cfbb472afc3dfef6b6
Geoffrey Hervet <hervetg@gmail.com>
2015-06-30 11:05:25 +0200

    Form search

---
+++ app/scripts/directives/searchbtn.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name angularApp.directive:SearchBtn
+ * @description
+ * # SearchBtn
+ */
+angular.module('angularApp')
+  .directive('searchBtn', function ($location) {
+    return {
+      templateUrl: 'views/directives/search-btn.html',
+      restrict: 'E',
+      link: function postLink(scope, element, attrs) {
+        scope.searchValue = '';
+        var el = angular.element('.right-menu-opener');
+        var form = angular.element(el.attr('href'));
+        el
+          .click(function(e){
+            form.show();
+            form.find('input').focus();
+            e.preventDefault();
+            return false;
+          })
+        ;
+        form
+          .find('.close-btn')
+          .click(function(e){
+            e.preventDefault();
+            form.hide();
+            return false;
+          })
+        ;
+
+        scope.goToSearch = function(){
+          $location.path('/search').search({q: scope.searchValue});
+          return false;
+        };
+      }
+    };
+  });
