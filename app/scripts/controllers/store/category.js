98c9a6afb126dc605ab558d5c0552c4454d3084b
Geoffrey Hervet <hervetg@gmail.com>
2015-07-16 17:18:14 +0200

    [Tracking] Category

--- app/scripts/controllers/store/category.js
+++ app/scripts/controllers/store/category.js
@@ -15,6 +15,7 @@ angular.module('angularApp')
     $scope.loaded     = false;
     $scope.infiniteLoading = false;
     $scope.infiniteDisabled = true;
+    $scope.productsTracking = null;
 
     if (isNaN($scope.categoryId)) {
       return $location.path('/');
@@ -38,6 +39,11 @@ angular.module('angularApp')
         $scope.category = category;
         $scope.title = category.category_info.title;
         $scope.products = category.products.item;
+        var productsTracking = [];
+        angular.forEach($scope.products, function(product){
+          productsTracking.push(product.entity_id);
+        });
+        $scope.productsTracking = productsTracking;
         $timeout(function(){$scope.infiniteDisabled = false;});
       }, function(){
         $scope.error = true;
