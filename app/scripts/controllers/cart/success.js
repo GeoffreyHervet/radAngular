70ba3126be7226f551ac34c92ca7619f9b18d660
Geoffrey Hervet <hervetg@gmail.com>
2015-07-16 15:36:32 +0200

    [Tracking] Cart-confirmation and success

--- app/scripts/controllers/cart/success.js
+++ app/scripts/controllers/cart/success.js
@@ -13,9 +13,10 @@ angular.module('angularApp')
     $scope.orderId = LocalStorage.get('order_id');
     $scope.num     = LocalStorage.get('increment_id');
 
-    //$scope.orderId = 528249;
-    //$scope.num     = 100449410;
+    //$scope.orderId = 574970;
+    //$scope.num     = 100449431;
 
+    $scope.success    = true;
     $scope.loading    = true;
     $scope.order      = null;
     $scope.error      = false;
@@ -28,22 +29,28 @@ angular.module('angularApp')
 
     order.get($scope.orderId).then(
       function (response){
-        $scope.loading = false;
         if (response.message && response.message.status === 'error') {
+          $scope.loading = false;
           return $scope.error = response.message.text;
         }
         if (response.order_details) {
           $scope.order = response.order_details;
-          $scope.items = Array.isArray($scope.order.ordered_items.item) ? $scope.order.ordered_items.item : [$scope.order.ordered_items.item];
+          $scope.items = Utils.arrayfy($scope.order.ordered_items.item);
+          $scope.ids = [];
+          angular.forEach($scope.items, function(item){
+            $scope.ids.push(item._product_id);
+          });
           $scope.totals = [];
           angular.forEach($scope.order.totals, function(v) {
             if (v.summary) v = v.summary;
             $scope.totals.push({label: v._label, val: v.__text});
           });
+          $scope.loading = false; // here for tracking
           return ;
 
         }
         $scope.error = 'error.connexion_lost';
+        $scope.loading = false;
       },
       function (){
         $scope.loading = false;
