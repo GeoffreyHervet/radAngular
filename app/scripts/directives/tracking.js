789abff077e5b3579f7bd812e8d9e8407739ffd6
Geoffrey Hervet <hervetg@gmail.com>
2015-07-17 15:01:56 +0200

    Fixes

--- app/scripts/directives/tracking.js
+++ app/scripts/directives/tracking.js
@@ -1,5 +1,24 @@
 'use strict';
 
+(function (){
+  var addScript = function(url,content){
+    var a = ['s','c','r','i','p','t'];
+    var str = '<' + a.join('');
+    if (url) {
+      str += ' src="' + url + '">';
+    }
+    else {
+      str += '>' + content;
+    }
+    str += '</' + a.join('') + '>';
+    document.write(str);
+  };
+
+  //addScript('//connect.facebook.net/en_US/fbds.js');
+  addScript('//platform.twitter.com/oct.js');
+  addScript(0, "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-31344840-1', 'auto');");
+})();
+
 /**
  * @ngdoc directive
  * @name angularApp.directive:tracking
