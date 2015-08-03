68d667255a3e5a920f8eed0ee21581114ab39f01
Geoffrey Hervet <hervetg@gmail.com>
2015-08-03 15:44:42 +0200

    Carousel

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,18 +10,21 @@
 angular.module('angularApp')
   .controller('MainCtrl', function ($scope, ApiLink, $cookies, $http, LocalStorage, Utils, $state) {
     window._state = $state;
-    $scope.inserts = LocalStorage.getObject('home/inserts');
+    $scope.inserts  = LocalStorage.getObject('home/inserts');
+    $scope.carousels = LocalStorage.getObject('home/carousel');
     $scope.Utils = Utils;
 
-    if (!$scope.inserts) {
+    if (!$scope.inserts && !$scope.carousels) {
       $scope.loading = true;
       $http
         .get(ApiLink.getApiBase() + '/raaad_xmlconnect/index/index/app_code/' + ApiLink.getAppCode() + '/level/1')
         .then(function(response) {
           $scope.loading = false;
           if (response.data && response.data.home && response.data.home.inserts) {
-            $scope.inserts = response.data.home.inserts.insert;
+            $scope.inserts = Utils.arrayfy(response.data.home.inserts.insert);
+            $scope.carousels = Utils.arrayfy(response.data.home.carousel.insert);
             LocalStorage.putObject('home/inserts', $scope.inserts);
+            LocalStorage.putObject('home/carousel', $scope.carousels);
           }
         }, function() {
           $scope.loading = false;
