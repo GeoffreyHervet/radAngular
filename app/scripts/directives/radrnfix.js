93dc8aff67b358695db68676925ad81ad937ef99
Geoffrey Hervet <hervetg@gmail.com>
2015-09-02 12:24:16 +0200

    Fix carousel height bug

--- app/scripts/directives/radrnfix.js
+++ app/scripts/directives/radrnfix.js
@@ -12,7 +12,11 @@ angular.module('angularApp')
       restrict: 'A',
       link: function postLink(scope, element) {
         var setSizes = function($ul) {
-          $ul.height($ul.find('img').css('width', '100%').eq(0).height());
+          var height = $ul.find('img').css('width', '100%').eq(0).height();
+          if (!height) {
+            height = 580;
+          }
+          $ul.height(height);
         };
         element.bind('load', function() {
           setSizes(element.closest('ul[rn-carousel]'));
