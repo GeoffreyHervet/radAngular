af1d5735431f3748fee9b27e58646ae9ecda61f1
Geoffrey Hervet <hervetg@gmail.com>
2015-10-23 17:32:04 +0200

    Fix fail

--- Gruntfile.js
+++ Gruntfile.js
@@ -254,8 +254,7 @@ module.exports = function (grunt) {
         flow: {
           html: {
             steps: {
-              //js: ['concat', 'uglifyjs'],
-              js: ['concat'],
+              js: ['concat', 'uglifyjs'],
               css: ['cssmin']
             },
             post: {}
@@ -525,7 +524,7 @@ module.exports = function (grunt) {
     'copy:dist',
     'cdnify',
     'cssmin',
-    //'uglify',
+    'uglify',
     'filerev',
     'usemin',
     'htmlmin',
